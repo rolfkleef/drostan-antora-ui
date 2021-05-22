d2428831b1bbc6ee734e0939a257fb9f3aa44eed
Rolf Kleef <rolf@drostan.org>
2021-05-22 21:33:34 +0200

    Upgrade prev/next via arrow keys
    - Use key instead of deprecated keyCode
    - Don't switch page when in a text input field

--- src/js/90-prev-next-keys.js
+++ src/js/90-prev-next-keys.js
@@ -1,15 +1,24 @@
-document.onkeydown = function (evt) {
-  evt = evt || window.event
-  switch (evt.keyCode) {
-    case 37:
-      if (document.querySelector('link[rel="prev"]')) {
-        window.location.assign(document.querySelector('link[rel="prev"]').href)
+;(function () {
+  'use strict'
+
+  /* Go to previous or next page with arrow keys (when not in a text input field) */
+  document.addEventListener('keydown', function (event) {
+    const notHere = ['INPUT', 'TEXTAREA']
+    const focusElement = document.activeElement.tagName.toUpperCase()
+
+    if (!(notHere.includes(focusElement))) {
+      switch (event.key) {
+        case 'ArrowLeft':
+          if (document.querySelector('link[rel="prev"]')) {
+            window.location.assign(document.querySelector('link[rel="prev"]').href)
+          }
+          break
+        case 'ArrowRight':
+          if (document.querySelector('link[rel="next"]')) {
+            window.location.assign(document.querySelector('link[rel="next"]').href)
+          }
+          break
       }
-      break
-    case 39:
-      if (document.querySelector('link[rel="next"]')) {
-        window.location.assign(document.querySelector('link[rel="next"]').href)
-      }
-      break
-  }
-}
+    }
+  }, false)
+})()
