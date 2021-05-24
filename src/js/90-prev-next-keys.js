9ddb9a0b6c9d73d1984a392816fc9959775ee4ea
Rolf Kleef <rolf@drostan.org>
2021-05-24 13:46:31 +0200

    Fix: don't switch page on left/right when text selected
    - Added modifier key detection: only do left/right when no shift etc pressed
    - Added k and p for previous, j and n for next (as in pdf viewer)
    - Found no way yet to detect if cursor navigation is enabled in a browser: for better accessibility it's possible to disable the page turn on arrow left/right
    
    (O'Reilly has the same behaviour now: left/right flip pages even in cursor navigation mode)

--- src/js/90-prev-next-keys.js
+++ src/js/90-prev-next-keys.js
@@ -3,17 +3,22 @@
 
   /* Go to previous or next page with arrow keys (when not in a text input field) */
   document.addEventListener('keydown', function (event) {
-    const notHere = ['INPUT', 'TEXTAREA']
-    const focusElement = document.activeElement.tagName.toUpperCase()
+    const focusElement = document.activeElement.tagName
+    const notHere = ['INPUT', 'TEXTAREA'].includes(focusElement.toUpperCase())
+    const keyModeOk = !(event.altKey | event.ctrlKey | event.shiftKey | event.metaKey)
 
-    if (!(notHere.includes(focusElement))) {
+    if (!notHere & keyModeOk) {
       switch (event.key) {
         case 'ArrowLeft':
+        case 'k':
+        case 'p':
           if (document.querySelector('link[rel="prev"]')) {
             window.location.assign(document.querySelector('link[rel="prev"]').href)
           }
           break
         case 'ArrowRight':
+        case 'j':
+        case 'n':
           if (document.querySelector('link[rel="next"]')) {
             window.location.assign(document.querySelector('link[rel="next"]').href)
           }
